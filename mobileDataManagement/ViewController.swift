577259c4164b82379c00609f82549eb5e142c0fe
sunil3696 <sunilstark3696@gmail.com>
2024-07-10 15:08:36 -0400

    Initial Commit

---
+++ mobileDataManagement/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mobileDataManagement
+//
+//  Created by Sunil Balami on 2024-07-10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
